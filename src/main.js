bc0f35099948aa8f595cc292a5af8183e732c8db
wuchao <2721177083@qq.com>
2020-06-28 17:29:40 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -18,8 +19,9 @@ axios.interceptors.request.use(config => {
   return config;
 })
 Vue.prototype.$http = axios
-
 Vue.config.productionTip = false
+
+Vue.component("tree-table",TreeTable);
 
 new Vue({
   router,
